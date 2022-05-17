69392258b8f3049c6f1c5a1f369daaa6172740a9
makewheels <33256842+makewheels@users.noreply.github.com>
2022-05-17 12:13:07 +0800

    init

---
+++ src/main/java/com/github/makewheels/msaliyunmarket/MsAliyunMarketApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.msaliyunmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAliyunMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAliyunMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/makewheels/msaliyunmarket/MsAliyunMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.msaliyunmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAliyunMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
